0b244c02e9cfd1a3ba3b3aab6b28957ace3d9eca
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-06-24 23:14:08 +0600

    block_backs.rb file code updated

--- block_backs.rb
+++ block_backs.rb
@@ -2,5 +2,16 @@
 3.times() do
 	puts "Echo!"	
 end
+puts "==========="
 # style 2
 4.times() { puts "Echo!" }
+puts "==========="
+# style 1
+# 5.times do |number|
+# 	puts "#{number} Echo!"
+# end
+5.times do |n|
+	puts "#{n} Echo!"
+end
+puts "==========="
+5.times { |n| puts "#{n} Echo!"}
