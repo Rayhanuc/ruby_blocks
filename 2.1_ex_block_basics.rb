d27bec0cb94af4e04f4228e7257f6a2f7a4b5252
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-06-27 21:03:50 +0600

    block scoping example

--- 2.1_ex_block_basics.rb
+++ 2.1_ex_block_basics.rb
@@ -6,8 +6,15 @@
 end
 puts "Another example"
 1.upto(5) { |count| 
-  puts "#{count} time  ======="
-  puts "#{count} situp"
-  puts "#{count} pushup"
-  puts "#{count} chinup"
+    puts "#{count} time  ======="
+    puts "#{count} situp"
+    puts "#{count} pushup"
+    puts "#{count} chinup"
 }
+puts "print number every 3 step"
+
+1.step(9, 3) do |count|
+    puts "#{count} situp"
+    puts "#{count} pushup"
+    puts "#{count} chinup"
+end
---
+++ 2.3_block_scoping.rb
@@ -0,0 +1,3 @@
+3.times do |number|
+  puts "#{number} Howdy!"
+end
